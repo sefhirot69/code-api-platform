9571095e01f300547da701067d614ff85246f235
sefhirot69 <josalillo@gmail.com>
2021-10-26 12:57:56 +0200

    orphanRemove true

--- start/src/Entity/User.php
+++ start/src/Entity/User.php
@@ -63,7 +63,7 @@ class User implements UserInterface
     private $username;
 
     /**
-     * @ORM\OneToMany(targetEntity=CheeseListing::class, mappedBy="owner", cascade={"persist"})
+     * @ORM\OneToMany(targetEntity=CheeseListing::class, mappedBy="owner", cascade={"persist"}, orphanRemoval=true)
      */
     #[Groups(['user:read', 'user:write'])]
     #[Assert\Valid]
